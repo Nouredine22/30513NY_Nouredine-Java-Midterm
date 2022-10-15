f9c14d144f4295c2b5ff85786b6eaeb398652500
Nouredine22 <dean.fedoul@gmail.com>
2022-10-14 20:18:32 -0400

    patern

--- src/math_problems/Pattern.java
+++ src/math_problems/Pattern.java
@@ -11,6 +11,31 @@ public class Pattern {
      */
 
     public static void main(String[] args) {
+        printPattern();
+
+    }
+
+    /**
+     * printPattern print a number patter
+     * 100,99,98,97,96,95,94,93,92,91,90,88,86,84,82,80,78,76,74,72,70,67,64,61,58,55,52,49,46,43,40,36,32...
+     */
+
+    static void printPattern() {
+
+        for (int i = 100; i >= 90; i--) {
+            System.out.print(i + ",");
+        }
+        for (int i = 88; i >= 70; i = i - 2) {  // visually i can see digits skips by 2
+            System.out.print(i + ",");
+        }
+        for (int i = 67; i >= 40; i = i - 3) {  // in here by 3
+            System.out.print(i + ",");
+        }
+        for (int i = 40; i >= 32; i = i - 4) {  // in here by 4
+            System.out.print(i + ",");
+        }
+        System.out.print("...");
+
 
     }
 }
