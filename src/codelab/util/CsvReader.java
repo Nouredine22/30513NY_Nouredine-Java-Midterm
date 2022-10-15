afaa8456218825719e74a52e1a050d52c202f978
Nouredine22 <dean.fedoul@gmail.com>
2022-10-15 19:10:04 -0400

    Csv reader lab code

--- src/codelab/util/CsvReader.java
+++ src/codelab/util/CsvReader.java
@@ -3,6 +3,7 @@ package codelab.util;
 import codelab.Student;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,8 +12,9 @@ import java.util.List;
 
 public class CsvReader {
 
-    /** INSTRUCTIONS
-     *
+    /**
+     * INSTRUCTIONS
+     * <p>
      * You will find a Comma-Separated Value (CSV) file within this package. It contains CodeLab status for each student
      * who registered for the CodeLab course.
      * Based on number of solution you solved in CodeLab, a message will be generated for you.
@@ -21,12 +23,16 @@ public class CsvReader {
 
     public static void main(String[] args) {
 
-        String csvFilePath = System.getProperty("user.dir") + "\\src\\codelab\\data\\roster.csv";
+
+
+
+
+        String csvFilePath = System.getProperty("user.dir") + File.separator + "src" + File.separator + "codelab"
+                + File.separator + "data" + File.separator + "roster.csv";
         String row;
         String csvSplitBy = ",";
         BufferedReader br;
         List<Student> roster = new ArrayList<>();
-
         try {
             br = new BufferedReader(new FileReader(csvFilePath));
             int lineNumber = 0;
@@ -42,13 +48,23 @@ public class CsvReader {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         Collections.sort(roster);
-
         for (Student student : roster) {
             System.out.println(convertNumberOfProblemsSolved(student));
         }
+
+        // average score = # of exercises solved / number of students
+        int totalExercisesSolved = 0;
+        int numOfStudents = 0;
+
+        for (Student student : roster) {
+            totalExercisesSolved += student.getNumberOfExercisesSolved();
+            numOfStudents++;
+        }
+
+        System.out.println("\nAverage Score: " + totalExercisesSolved / numOfStudents);
     }
+
 
     private static String convertNumberOfProblemsSolved(Student student) {
         String name = student.getFirstName();
@@ -67,4 +83,25 @@ public class CsvReader {
             return "Very low effort. Not a good sign, " + name;
         }
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
