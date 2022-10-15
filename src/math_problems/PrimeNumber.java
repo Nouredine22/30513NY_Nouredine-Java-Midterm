2b81fda7db4dff1d1d72e57047a97a14d943d6fd
Nouredine22 <dean.fedoul@gmail.com>
2022-10-14 20:38:58 -0400

    prime number

--- src/math_problems/PrimeNumber.java
+++ src/math_problems/PrimeNumber.java
@@ -11,7 +11,31 @@ public class PrimeNumber {
 
 
     public static void main(String[] args) {
+        printPrim(2,1000000);
+
+    }
+
+//   print prime number begin int start to int end
+
+    static void printPrim(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) System.out.println(i + " ");
+        }
+    }
+
+//    isPrime method return true if int number is prime
+
+    private static boolean isPrime(int number) {
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {//O(sqrt(n))
+            if (number % i == 0) return false;
+        }
+
+        return true;
 
     }
 
 }
+
+
+
